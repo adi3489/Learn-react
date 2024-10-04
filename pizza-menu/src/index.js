b4d12133226e2b31f97705e28f47ae32f12fbc36
adi3489 <07aditya.cn@gmail.com>
2024-10-04 20:36:40 +0530

    props

--- pizza-menu/src/index.js
+++ pizza-menu/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
+import "./index.css";
 const pizzaData = [
   {
     name: "Focaccia",
@@ -48,24 +48,73 @@ const pizzaData = [
 
 function App() {
   return (
-    <div>
-      <h1>Hello react!</h1>
-      <Pizza />
-      <Pizza />
-      <Pizza />
+    <div className="container">
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  );
+}
+function Header() {
+  const style = {};
+  // const style = { color: "red", fontSize: "48px", textTransform: "uppercase" };
+  return (
+    <header className="header">
+      <h1 style={style}>Fast React Pizza Co.</h1>
+    </header>
+  );
+}
+
+function Menu() {
+  return (
+    <main className="menu">
+      <h2>Our menu</h2>
+      <Pizza
+        name="pizza spinaci"
+        ingredients="Tomato, mozarella, spinach, and ricotta cheese"
+        photoName="pizzas/spinaci.jpg"
+        price={10}
+      />
+      <Pizza
+        name="Pizza Funghi"
+        ingredients="Tomato,mushrooms"
+        price={12}
+        photoName="pizzas/funghi.jpg"
+      />
+    </main>
+  );
+}
+function Pizza(props) {
+  console.log(props);
+  return (
+    <div className="pizza">
+      <img src={props.photoName} alt={props.name} />
+      <div>
+        <h3> {props.name}</h3>
+        <p>{props.ingredients}</p>
+        <span>{props.price + 3}</span>
+      </div>
     </div>
   );
 }
 
-function Pizza() {
+function Footer() {
+  const hour = new Date().getHours();
+  const openHour = 12;
+  const closeHour = 22;
+
+  const isOpen = hour >= openHour && hour <= closeHour;
+  console.log(isOpen);
+  // if (hour >= openHour && hour <= closeHour) alert("We're currentlyopen!");
+  // else alert("Sorry we closed");
   return (
-    <div>
-      <img src="pizzas/spinaci.jpg" alt="pizza" />
-      <h2> Pizza Spinaci</h2>
-      <p>Tomato, mozarella, spinach, and ricotta cheese</p>
-    </div>
+    <footer className="footer">
+      {new Date().toLocaleTimeString()}.We're currently open
+    </footer>
   );
+  //return React.createElement("footer", null, "we're currently open");
 }
+
 //React v18
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
