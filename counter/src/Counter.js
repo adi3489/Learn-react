7f0812dcef051e4f2b9a59b5acaaee83b30d8acb
adi3489 <07aditya.cn@gmail.com>
2024-10-28 21:44:14 +0530

    new project

--- counter/src/Counter.js
+++ counter/src/Counter.js
@@ -2,14 +2,19 @@ import { useState } from "react";
 import Step from "./Step";
 function Counter() {
   const [count, setCount] = useState(0);
+  const [step, setStep] = useState(1);
   const date = new Date("October 28 2024");
   date.setDate(date.getDate() + count);
   return (
     <div>
-      <Step />
-      <button onClick={() => setCount(count - 1)}>-</button>Count:
+      <div>
+        <button onClick={() => setStep(step - 1)}>-</button>
+        Step:{step}
+        <button onClick={() => setStep(step + 1)}>+</button>
+      </div>
+      <button onClick={() => setCount(count - step)}>-</button>Count:
       {count}
-      <button onClick={() => setCount(count + 1)}>+</button>
+      <button onClick={() => setCount(count + step)}>+</button>
       <p>
         <span>
           {count === 0
